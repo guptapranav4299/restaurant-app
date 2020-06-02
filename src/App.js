f1202bd0d382c98d2f6394b5bad9b4fcda4859f7
Pranav Gupta <guptapranav4299@gmail.com>
2020-06-02 23:58:09 +0530

    final-changes

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import Main from './Components/MainComponent';
 import './App.css';
 import Dishdetail from './Components/DishdetailComponent';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {ConfigureStore} from './redux/ConfigureStore';
+
+
+const store=ConfigureStore();
 
 class App extends React.Component {
 
   render(){
     return (
+      <Provider store={store}>
       <BrowserRouter>
       <div>
         <Main></Main>
 
       </div>
       </BrowserRouter>
+      </Provider>
     );
   }
   
